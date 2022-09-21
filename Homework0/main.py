c9866768f9130b9825aefb1cd1fdc4ec4f3a7f8d
Jogden330 <71284561+Jogden330@users.noreply.github.com>
2022-09-21 09:59:00 -0700

    more work on homework 0

--- Homework0/main.py
+++ Homework0/main.py
@@ -6,14 +6,40 @@ Created on Tue Sep 20 19:21:33 2022
 """
 import random
 
-numbs = 4*[0]
+numbsToGuess = 4*[0]
 wins = 0
 loss = 0
+output = ''
 
 #creat random numbers
-for i in range(len(numbs)):
-    numbs[i] = random.randint(0,5)
-    print(numbs[i])
+for i in range(len(numbsToGuess)):
+    numbsToGuess[i] = random.randint(0,5)
+    print(numbsToGuess[i])
 
+print('wins ', wins,'loss ', loss)
 
-def surch_len 
+print('Enter 4 numbers between 0 and 5')
+
+input_Numbers = input()
+
+input_Numbers  = input_Numbers.split() 
+
+for j in range(len(input_Numbers)):
+    try:
+       number = int(input_Numbers[j]) 
+       for i in range(len(numbsToGuess)):
+           if (numbsToGuess[i] == number) and (i == j) :
+               output = output + "+"
+               break
+           else :
+               if (numbsToGuess[i] == number) :
+                   output = output + "-"
+                   break
+                   
+       
+    except ValueError:
+         print(number, " is not a number")
+     
+print(output)
+    
+

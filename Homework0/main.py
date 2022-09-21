5ac88f515c3d665170fd8d9177090ffbb070ce61
Jogden330 <71284561+Jogden330@users.noreply.github.com>
2022-09-20 19:46:16 -0700

    started homework

---
+++ Homework0/main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 20 19:21:33 2022
+
+@author: jogde
+"""
+import random
+
+numbs = 4*[0]
+wins = 0
+loss = 0
+
+#creat random numbers
+for i in range(len(numbs)):
+    numbs[i] = random.randint(0,5)
+    print(numbs[i])
+
+
+def surch_len 
